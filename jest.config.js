9294e4418c643abf93911fb67b0da208a9eb88c1
Emmanuel Ajayi <emmanuelajayi380@yahoo.co.uk>
2025-03-20 09:46:46 +0000

    feat: add jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-    transform: {
-      "^.+\\.(js|jsx)$": "babel-jest",
-    },
-  };
-  
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.svg$": "<rootDir>/__mocks__/svgMock.js"
+  }
+};
